d726693df4e0119a250e36d67cf62a12dd2605c1
diwuh <1581176417@qq.com>
2018-02-08 15:14:18 +0800

    功能基本差不多了

--- config/services.php
+++ config/services.php
@@ -15,8 +15,8 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+        'domain' => env('MAILGUN_DOMAIN','your-mailgun-domain'),
+        'secret' => env('MAILGUN_SECRET','your-mailgun-key'),
     ],
 
     'ses' => [
