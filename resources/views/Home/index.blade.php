0f01592c9e5cad8f1ff95ec283ab33ea0c9330d2
diwuh <1581176417@qq.com>
2018-04-20 16:51:04 +0800

    调了一点样式

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -11,30 +11,53 @@
             {!! $title !!}
         @endif
         @foreach($list as $list)
-        <article class="col-lg-11 table-bordered " style="padding-top: 10px;margin-top:15px;background: white">
-            <div class="col-lg-12" style="padding-bottom: 15px " title="{{$list->title}}" target="_blank">
+        <article class="col-lg-11 table-bordered " style="padding-top: 1px;margin-top:15px;background: white">
+            <div class="col-md-4" style="padding: 2% 0 3% 0">
+                <img class=" img-thumbnail aft"
+                     src="{{$list->back==1?config('app.img').$list->pic:asset($list->pic)}}" alt="{{$list->title}}"
+                     style="display: inline;">
+            </div>
+            <div class="col-md-8">
                 <div class="col-md-12" style="">
                     <a href="{{url('/content/'.$list->id)}}" class="col-md-12 h3 text-primary">{{$list->title}}</a>
                 </div>
                 <div class="col-md-12" >
-                    <div class="col-md-4"><span class="glyphicon glyphicon-user"></span>余温</div>
-                    <div class="col-md-5"><span class="glyphicon glyphicon-time"></span>{{date('Y年m月d日 H时i分',$list->time)}}</div>
+                    <div class="col-md-3"><span class="glyphicon glyphicon-user"></span>余温</div>
+                    <div class="col-md-6"><span class="glyphicon glyphicon-time"></span>{{date('Y年m月d日H时i',$list->time)}}</div>
                     <div class="col-md-3"><span class="glyphicon glyphicon-list-alt"></span> &nbsp;{{$list->cateid}}</div>
                 </div>
                 <div class="col-md-12" style="padding-top: 15px;position: relative;">
-                    <div style="height: 100px" class="col-md-4">
-                        <img class=" img-thumbnail aft"
-                             src="{{$list->back==1?config('app.img').$list->pic:asset($list->pic)}}" alt="{{$list->title}}" class="thumb"
-                             style="display: inline; height: 100px">
+                    <div class="col-md-9" style="margin-top: 3%">
+                        {{str_limit($list->desc,100)}}
                     </div>
-                    <div class="col-md-5" style="margin-top: 3%">
-                        <p>
-                            {{str_limit($list->desc,100)}}
-                        </p>
+                    <div class="col-md-3">
+                        <a href="{{url('/content/'.$list->id)}}" class="btn btn-primary" style="color: white;">查看原文</a>
                     </div>
-                    <a href="{{url('/content/'.$list->id)}}" class="text-center col-md-3  btn btn-primary" style="color: white;position: absolute; bottom: 0;">查看原文</a>
                 </div>
             </div>
+            {{--<div class="col-lg-12" style="padding-bottom: 15px " title="{{$list->title}}" target="_blank">--}}
+                {{--<div class="col-md-12" style="">--}}
+                    {{--<a href="{{url('/content/'.$list->id)}}" class="col-md-12 h3 text-primary">{{$list->title}}</a>--}}
+                {{--</div>--}}
+                {{--<div class="col-md-12" >--}}
+                    {{--<div class="col-md-4"><span class="glyphicon glyphicon-user"></span>余温</div>--}}
+                    {{--<div class="col-md-5"><span class="glyphicon glyphicon-time"></span>{{date('Y年m月d日 H时i分',$list->time)}}</div>--}}
+                    {{--<div class="col-md-3"><span class="glyphicon glyphicon-list-alt"></span> &nbsp;{{$list->cateid}}</div>--}}
+                {{--</div>--}}
+                {{--<div class="col-md-12" style="padding-top: 15px;position: relative;">--}}
+                    {{--<div style="height: 100px" class="col-md-4">--}}
+                        {{--<img class=" img-thumbnail aft"--}}
+                             {{--src="{{$list->back==1?config('app.img').$list->pic:asset($list->pic)}}" alt="{{$list->title}}" class="thumb"--}}
+                             {{--style="display: inline; height: 100px">--}}
+                    {{--</div>--}}
+                    {{--<div class="col-md-5" style="margin-top: 3%">--}}
+                        {{--<p>--}}
+                            {{--{{str_limit($list->desc,100)}}--}}
+                        {{--</p>--}}
+                    {{--</div>--}}
+                    {{--<a href="{{url('/content/'.$list->id)}}" class="text-center col-md-3  btn btn-primary" style="color: white;position: absolute; bottom: 0;">查看原文</a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
 
         </article>
         @endforeach
